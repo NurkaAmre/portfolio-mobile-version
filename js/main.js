564b4d7ffc67b083910724a2b5971b89003ff86b
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-13 14:23:25 +0600

    Fix linter js errors

--- js/main.js
+++ js/main.js
@@ -5,17 +5,16 @@ const extBtn = document.querySelector('.exit');
 function show() {
   openMenu.style.display = 'block';
   openMenu.classList.toggle('open');
-  hamburger.style.visibility ='hidden';
-  extBtn.style.visibility ='visible';
-};
+  hamburger.style.visibility = 'hidden';
+  extBtn.style.visibility = 'visible';
+}
 
 function close() {
   openMenu.style.display = 'none';
   openMenu.classList.remove('open');
-  hamburger.style.visibility ='visible';
-  extBtn.style.visibility ='hidden';
-};
+  hamburger.style.visibility = 'visible';
+  extBtn.style.visibility = 'hidden';
+}
 
 hamburger.addEventListener('click', show);
 extBtn.addEventListener('click', close);
-
