e6a05962228e83cbed2c312e2d3482e80bb1c6aa
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-13 14:20:33 +0600

    Fix linter errors

--- js/main.js
+++ js/main.js
@@ -2,19 +2,20 @@ const hamburger = document.querySelector('.hamburger');
 const openMenu = document.querySelector('.mobile-option');
 const extBtn = document.querySelector('.exit');
 
-hamburger.addEventListener('click', show);
-extBtn.addEventListener('click', close);
-
-function show () {
+function show() {
   openMenu.style.display = 'block';
   openMenu.classList.toggle('open');
   hamburger.style.visibility ='hidden';
   extBtn.style.visibility ='visible';
 };
 
-function close () {
+function close() {
   openMenu.style.display = 'none';
   openMenu.classList.remove('open');
   hamburger.style.visibility ='visible';
   extBtn.style.visibility ='hidden';
 };
+
+hamburger.addEventListener('click', show);
+extBtn.addEventListener('click', close);
+
