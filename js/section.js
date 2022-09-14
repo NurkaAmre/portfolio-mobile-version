439c29cd9899d73f92c609af698f094da7c85ff4
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-14 15:53:07 +0600

    Pop-up created

---
+++ js/section.js
@@ -0,0 +1,266 @@
+const projects = [
+    {
+        id: '1',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle21.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle21.svg',
+            './assets/images/Rectangle21.svg',
+            './assets/images/Rectangle21.svg',
+            './assets/images/Rectangle21.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+
+    {
+        id: '2',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle22.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle22.svg',
+            './assets/images/Rectangle22.svg',
+            './assets/images/Rectangle22.svg',
+            './assets/images/Rectangle22.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+
+    {
+        id: '3',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle23.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle23.svg',
+            './assets/images/Rectangle23.svg',
+            './assets/images/Rectangle23.svg',
+            './assets/images/Rectangle23.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+
+    {
+        id: '4',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle24.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle24.svg',
+            './assets/images/Rectangle24.svg',
+            './assets/images/Rectangle24.svg',
+            './assets/images/Rectangle24.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+
+    {
+        id: '5',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle25.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle25.svg',
+            './assets/images/Rectangle25.svg',
+            './assets/images/Rectangle25.svg',
+            './assets/images/Rectangle25.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+
+    {
+        id: '6',
+        title: 'Project name goes here',
+        features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
+        image: './assets/images/Rectangle26.svg',
+        img: './assets/images/Union.svg',
+        sideImage: [
+            './assets/images/Rectangle26.svg',
+            './assets/images/Rectangle26.svg',
+            './assets/images/Rectangle26.svg',
+            './assets/images/Rectangle26.svg',
+        ],
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+    enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    <br /><br />
+    nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+    amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi.`,
+        linkToSource: 'https://nurkaamre.github.io/portfolio-mobile-version/',
+        linkToLive: 'https://github.com/NurkaAmre',
+        btn: 'See Source',
+        side: ['Next Project', 'Previous Project'],
+        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+    },
+];
+
+const body = document.querySelector('#project');
+
+for (i = 0; i < projects.length; i += 1) {
+    const projectSection = document.createElement('section');
+    body.appendChild(projectSection);
+    projectSection.innerHTML = ` 
+    <div class="container container-information bottom">
+    <li class="span-3 project-item">
+            <img
+              class="project-img"
+              src="${projects[i].image}"
+              alt="laptop"
+            />
+            <div class="container container-information bottom">
+              <h2 class="heading heading-desktop">${projects[i].title}</h2> 
+              <ul class="features">
+                <li class="feature feature-desktop">${projects[i].features[0]}</li>
+                <li class="feature feature-desktop">${projects[i].features[1]}</li>
+                <li class="feature feature-desktop">${projects[i].features[2]}</li>
+              </ul>
+              <button class="c-projects see-source">
+                ${projects[i].btn}
+                <img src="${projects[i].img}" alt="arrow" />
+              </button>
+            </div>
+        </li>
+    </div>
+    `;
+};
+
+const open = document.querySelectorAll('.see-source');
+console.log(open);
+for (let j = 0; j < open.length; j += 1) {
+    open[j].addEventListener('click', () => {
+        const main = document.createElement('div');
+        main.className = 'modal-container';
+        const modal = document.createElement('div');
+        const item = target.getAttribute('id');
+
+        modal.innerHTML = `
+    <div class="container container-information bottom">
+    <div class="for-x">
+    <h2 class="heading heading-desktop modal-heading">${projects[item].title}</h2>
+    <button type="button" class="menu-btn">
+              &#x2716
+            </button>
+    </div>
+    <ul class="features modal-features">
+      <li class="feature feature-desktop feature-modal">${projects[item].features[0]}</li>
+      <li class="feature feature-desktop feature-modal">${projects[item].features[1]}</li>
+      <li class="feature feature-desktop feature-modal">${projects[item].features[2]}</li>
+    </ul>
+    <div>
+    <img class="modal-img" src="${projects[item].image}" alt="laptop-img" />
+    </div>
+    <ul class="previous-next list-img" role="list">
+              <li>
+                <img src="${projects[item].sideImage[0]}" alt="project" />
+              </li>
+              <li>
+                <img src="${projects[item].sideImage[1]}" alt="project" />
+              </li>
+              <li>
+                <img src="${projects[item].sideImage[2]}" alt="project" />
+              </li>
+              <li>
+                <img src="${projects[item].sideImage[3]}" alt="project" />
+              </li>
+            </ul>
+    <div>
+       <div class="modal-text"><p>${projects[item].description}</p></div>
+    <div class="modal-links">
+       <button class="c-projects see-more">
+         ${projects[item].btn}
+        <img src="${projects[item].icons[1]}" alt="arrow" />
+       </button>
+       <button class="c-projects see-more">
+         ${projects[i].btn}
+        <img src="${projects[item].icons[0]}" alt="arrow" />
+        </button>
+    </div>
+    <div>
+        <button>
+         <span>${projects[item].side[0]}</span>
+        </button>
+              <button>
+                <span>${projects[item].side[1]}</span>
+              </button>
+        </div>
+        </div>
+    </div>
+    `;
+
+        main.appendChild(modal);
+        projectSection.appendChild(main);
+
+        const close = document.querySelector('.menu-btn');
+        close.addEventListener('click', () => {
+            projectSection.removeChild(modal);
+        });
+});
+};
