41f64bc96076f61d8a5ed126bb0d719cedbb3756
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-15 21:41:46 +0600

    Add html, css

--- js/section.js
+++ js/section.js
@@ -1,10 +1,11 @@
-const projects = [
-    {
+const projects = [{
         id: '1',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle21.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
+        span: '3',
         sideImage: [
             './assets/images/Rectangle21.svg',
             './assets/images/Rectangle21.svg',
@@ -23,15 +24,15 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
-    },
-
-    {
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
+    }, {
         id: '2',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle22.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
         sideImage: [
             './assets/images/Rectangle22.svg',
             './assets/images/Rectangle22.svg',
@@ -50,15 +51,15 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
-    },
-
-    {
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
+    }, {
         id: '3',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle23.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
         sideImage: [
             './assets/images/Rectangle23.svg',
             './assets/images/Rectangle23.svg',
@@ -77,15 +78,15 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
-    },
-
-    {
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
+    }, {
         id: '4',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle24.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
         sideImage: [
             './assets/images/Rectangle24.svg',
             './assets/images/Rectangle24.svg',
@@ -104,15 +105,15 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
-    },
-
-    {
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
+    }, {
         id: '5',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle25.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
         sideImage: [
             './assets/images/Rectangle25.svg',
             './assets/images/Rectangle25.svg',
@@ -131,15 +132,15 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
-    },
-
-    {
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
+    },  {
         id: '6',
         title: 'Project name goes here',
         features: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
         image: './assets/images/Rectangle26.svg',
         img: './assets/images/Union.svg',
+        sideArrows: ['./assets/icons/left.png', './assets/icons/right.png'],
         sideImage: [
             './assets/images/Rectangle26.svg',
             './assets/images/Rectangle26.svg',
@@ -158,9 +159,10 @@ const projects = [
         linkToLive: 'https://github.com/NurkaAmre',
         btn: 'See Source',
         side: ['Next Project', 'Previous Project'],
-        icons: ['./assets/im3/github.png', './assets/im3/Union.png']
+        icons: ['./assets/icons/Vector.png', './assets/icons/Union.png'],
+        btnForPopup: ['See Live','See Source']
     },
-];
+    ];
 
 const body = document.querySelector('#project');
 
@@ -168,8 +170,8 @@ for (i = 0; i < projects.length; i += 1) {
     const projectSection = document.createElement('section');
     body.appendChild(projectSection);
     projectSection.innerHTML = ` 
-    <div class="container container-information bottom">
-    <li class="span-3 project-item">
+    <div class="container container-information bottom modal-b">
+    <li class="span-3" project-item desktop-grid">
             <img
               class="project-img"
               src="${projects[i].image}"
@@ -182,7 +184,7 @@ for (i = 0; i < projects.length; i += 1) {
                 <li class="feature feature-desktop">${projects[i].features[1]}</li>
                 <li class="feature feature-desktop">${projects[i].features[2]}</li>
               </ul>
-              <button class="c-projects see-source">
+              <button class="c-projects see-source" id=${projects[i].id} onclick="popUp(this.id)">
                 ${projects[i].btn}
                 <img src="${projects[i].img}" alt="arrow" />
               </button>
@@ -191,56 +193,59 @@ for (i = 0; i < projects.length; i += 1) {
     </div>
     `;
 };
+projects[2].style.gridSpan= "2";
 
-const open = document.querySelectorAll('.see-source');
-
-for (let j = 0; j < open.length; j += 1) {
-    open[j].addEventListener('click', () => {
-        const main = document.createElement('div');
-        main.className = 'modal-container';
-        const modal = document.createElement('div');
-        const item = target.getAttribute('id');
-
-        modal.innerHTML = `
-    <div class="container container-information bottom">
+function popUp (item) {
+    console.log(item)
+    const overlay = document.querySelector('.extra');
+    
+    overlay.style.display = 'flex'
+for (let j = 0; j < projects.length; j += 1) {
+    if (item.toString() === projects[j].id.toString()) {
+        const popupModal = document.querySelector('.popup-modal')
+        console.log(popupModal)
+        popupModal.innerHTML = `
+    <div class="container bottom ">
     <div class="for-x">
-    <h2 class="heading heading-desktop modal-heading">${projects[item].title}</h2>
+    <h2 class="modal-heading">${projects[item].title}</h2>
     <button type="button" class="menu-btn">
               &#x2716
             </button>
     </div>
-    <ul class="features modal-features">
-      <li class="feature feature-desktop feature-modal">${projects[item].features[0]}</li>
-      <li class="feature feature-desktop feature-modal">${projects[item].features[1]}</li>
-      <li class="feature feature-desktop feature-modal">${projects[item].features[2]}</li>
+    <ul class="modal-features">
+      <li class="feature-modal">${projects[item].features[0]}</li>
+      <li class="feature-modal">${projects[item].features[1]}</li>
+      <li class="feature-modal">${projects[item].features[2]}</li>
     </ul>
-    <div>
+    <div class="next-pre">
     <img class="modal-img" src="${projects[item].image}" alt="laptop-img" />
+    <img class="next" src="${projects[item].sideArrows[0]}" alt="previous"} />
+    <img class="pre" src="${projects[item].sideArrows[1]}" alt="next"} />
     </div>
-    <ul class="previous-next list-img" role="list">
-              <li>
-                <img src="${projects[item].sideImage[0]}" alt="project" />
+    <ul class="list-img" role="list">
+              <li role="listitem" class="choose">
+                <img class="img1" src="${projects[item].sideImage[0]}" alt="project" />
               </li>
-              <li>
-                <img src="${projects[item].sideImage[1]}" alt="project" />
+              <li role="listitem">
+                <img class="img1" src="${projects[item].sideImage[1]}" alt="project" />
               </li>
-              <li>
-                <img src="${projects[item].sideImage[2]}" alt="project" />
+              <li role="listitem">
+                <img class="img1" src="${projects[item].sideImage[2]}" alt="project" />
               </li>
-              <li>
-                <img src="${projects[item].sideImage[3]}" alt="project" />
+              <li role="listitem">
+                <img class="img1" src="${projects[item].sideImage[3]}" alt="project" />
               </li>
             </ul>
     <div>
        <div class="modal-text"><p>${projects[item].description}</p></div>
     <div class="modal-links">
-       <button class="c-projects see-more">
-         ${projects[item].btn}
-        <img src="${projects[item].icons[1]}" alt="arrow" />
+       <button class="see-more">
+         ${projects[item].btnForPopup[0]}
+        <img class="arrow" src="${projects[item].icons[1]}" alt="arrow" />
        </button>
-       <button class="c-projects see-more">
-         ${projects[i].btn}
-        <img src="${projects[item].icons[0]}" alt="arrow" />
+       <button class="see-more">
+         ${projects[item].btnForPopup[1]}
+        <img class="arrow" src="${projects[item].icons[0]}" alt="arrow" />
         </button>
     </div>
     <div>
@@ -254,13 +259,8 @@ for (let j = 0; j < open.length; j += 1) {
         </div>
     </div>
     `;
+    }
+}};
 
-        main.appendChild(modal);
-        projectSection.appendChild(main);
-
-        const close = document.querySelector('.menu-btn');
-        close.addEventListener('click', () => {
-            projectSection.removeChild(modal);
-        });
-});
-};
+// const projectItem = document.querySelectorAll('.project-item');
+// console.log(projectItem[0]);
